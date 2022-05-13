120cf124d9564526566a7c08dc99461e09b3cdd3
Ansab Sidiki <ansab@sidiki.io>
2022-05-12 20:40:24 -0400

    wrote food routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,14 @@
 const router = require('express').Router();
 
 const userRoutes = require('./user-routes');
+const foodRoutes = require('./food-routes');
+// const driverRoutes = require('./driver-routes');
+// const orderRoutes = require('./order-routes');
 
 router.use('/users', userRoutes);
+router.use('/food', foodRoutes);
+// router.use('/drivers', driverRoutes);
+// router.use('/orders', orderRoutes);
+
 
 module.exports = router;
