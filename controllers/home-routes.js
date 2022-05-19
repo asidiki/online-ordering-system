1dc585e55f8a7c627628af72c7b2f88579e447a5
Ansab Sidiki <ansab@sidiki.io>
2022-05-19 01:37:14 -0400

    passport auth added, all auth routes working

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,15 +1,17 @@
 const router = require('express').Router();
 
+
+
 router.get('/', (req, res) => {
     console.log(req.session);
     res.render('homepage');
 });
 
 router.get('/login', (req, res) => {
-    // if (req.session.loggedIn) {
-    //     res.redirect('/');
-    //     return;
-    // }
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
     res.render('login');
 });
 
--- server.js
+++ server.js
@@ -1,19 +1,17 @@
-const path = require('path');
-const express = require('express');
-const session = require('express-session');
-const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
-const fs = require('fs');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
-const css = fs.readFileSync('./public/assets/style.css', 'utf-8');
-const loginJS = fs.readFileSync('./public/javascript/login.js', 'utf-8')
-
-const routes = require('./controllers');
-const sequelize = require('./config/connection');
-// const helpers = require('./utils/helpers');
+const path          = require('path');
+const express       = require('express');
+const session       = require('express-session');
+const exphbs        = require('express-handlebars');
+const fs            = require('fs');
+const flash         = require('express-flash');
+const passport      = require('passport');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
 const sess = {
     secret: 'Super secret secret',
     cookie: {},
@@ -24,17 +22,27 @@ const sess = {
     })
 };
 
+app.use(passport.initialize());
 app.use(session(sess));
-app.use(express.json());
-app.use(routes);
-app.use(express.static(path.join(__dirname, 'public')));
+
+const hbs = exphbs.create({});
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(flash());
 
 
+const css = fs.readFileSync('./public/assets/style.css', 'utf-8');
+const loginJS = fs.readFileSync('./public/javascript/login.js', 'utf-8')
+const logoutJS = fs.readFileSync('./public/javascript/logout.js', 'utf-8')
 
+app.use(require('./controllers/'));
+
+// const helpers = require('./utils/helpers');
 
 
 sequelize.sync({ force: false }).then(() => {
