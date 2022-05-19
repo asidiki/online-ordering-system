97cb47668cce12a13e5d25ba68a85393b3c21873
Ansab Sidiki <ansab@sidiki.io>
2022-05-19 18:26:42 -0400

    auth logic with new homepage

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,5 +1,49 @@
 const router = require('express').Router();
 const { User } = require('../../models');
+const session = require('express-session');
+const bcrypt = require('bcrypt');
+const passport = require('passport');
+const localStrategy = require('passport-local').Strategy;
+
+
+
+passport.use(new localStrategy(function (email, password, done){
+    User.findOne({ username: email }, function(err, user){
+        if (err) return done(err);
+        if (!user) {
+            return done(null, false, { message: 'Incorrect Username' });
+        }
+        bcrypt.compare(password, user.password, function(err, res){
+            if (err) return done(err);
+            if (res === false) return done(null, false, { message: 'Incorrect password' });
+
+            return done(err, user);
+        })
+    })
+}))
+
+function success(req, res) {
+    const session = require('express-session')
+    req.session.save(() => {
+        console.log(dbUserData);
+        req.session.user_id = dbUserData.id;
+        req.session.username = dbUserData.username;
+        req.session.loggedIn = true;
+    
+        res.json(dbUserData);
+    });
+}
+
+
+passport.serializeUser(function (user_id, done){
+    done(null, user_id)
+});
+
+passport.deserializeUser(function (user_id, done){
+    User.findByPk(user_id, function(err, user_id){
+        done(err, user_id);
+    });
+});
 
 // GET /api/users
 router.get('/', (req, res) => {
@@ -34,49 +78,53 @@ router.get('/:id', (req, res) => {
 
 // POST /api/users
 router.post('/', (req, res) => {
-    // expects {first_name: 'Lernantino', last_name: 'Smith', email: 'lernantino@gmail.com', address: 'California'}
-    User.create({
-        first_name: req.body.first_name,
-        last_name: req.body.last_name,
-        email: req.body.email,
-        address: req.body.address
-    })
-        .then(dbUserData => res.json(dbUserData))
-        .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-        });
+    bcrypt.genSalt(10, (err, salt) => {
+        if (err) return next(err);
+        bcrypt.hash(req.body.password, salt, function (err, hash){
+            if (err) return next(err);
+            User.create({
+                first_name: req.body.first_name,
+                last_name: req.body.last_name,
+                email: req.body.email,
+                address: req.body.address,
+                password: hash,
+                username: req.body.username
+            }) .then(dbUserData => {
+                    req.session.save(() => {
+                        console.log(dbUserData);
+                        req.session.user_id = dbUserData.id;
+                        req.session.username = dbUserData.username;
+                        req.session.loggedIn = true;
+                    
+                        res.json(dbUserData);
+                    });
+            }) .catch(err => {
+                console.log(err);
+                res.status(500).json(err);
+            });
+        })
+        
+    }); 
 });
 
-// router.post('/login', (req, res) => {
-//     User.findOne({
-//       where: {
-//         email: req.body.email
-//       }
-//     }).then(dbUserData => {
-//       if (!dbUserData) {
-//         res.status(400).json({ message: 'No user with that email address!' });
-//         return;
-//       }
 
-//       const validPassword = dbUserData.checkPassword(req.body.password);
+router.post('/login', passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login'
+}));
 
-//       if (!validPassword) {
-//         res.status(400).json({ message: 'Incorrect password!' });
-//         return;
-//       }
-
-//       req.session.save(() => {
-//         // declare session variables
-//         req.session.user_id = dbUserData.id;
-//         req.session.username = dbUserData.username;
-//         req.session.loggedIn = true;
-
-//         res.json({ user: dbUserData, message: 'You are now logged in!' });
-//       });
-//     });
-//   });
+router.post('/logout', function (req, res){
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    }
+    else {
+        res.status(404).end();
+    }
+});
 
 
-
-module.exports = router;
+module.exports = [
+    router
+]
--- server.js
+++ server.js
@@ -1,19 +1,47 @@
-const path = require('path');
-const express = require('express');
-// const session = require('express-session');
-// const exphbs = require('express-handlebars');
-// const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
-const routes = require('./controllers');
-const sequelize = require('./config/connection');
-// const helpers = require('./utils/helpers');
+const path          = require('path');
+const express       = require('express');
+const session       = require('express-session');
+const exphbs        = require('express-handlebars');
+const fs            = require('fs');
+const flash         = require('express-flash');
+const passport      = require('passport');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// app.use(session(sess));
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+app.use(passport.initialize());
+app.use(session(sess));
+
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
-app.use(routes);
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(flash());
+
+
+const css = fs.readFileSync('./assets/style.css', 'utf-8');
+// const loginJS = fs.readFileSync('./public/javascript/login.js', 'utf-8')
+// const logoutJS = fs.readFileSync('./public/javascript/logout.js', 'utf-8')
+
+app.use(require('./controllers/'));
+
 
 
 sequelize.sync({ force: false }).then(() => {
