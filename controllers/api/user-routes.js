30964cc35c69decb4c42feb2d2922505fcd3c5c2
Ansab Sidiki <ansab@sidiki.io>
2022-05-21 16:39:46 -0400

    login function fixed

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,49 +1,6 @@
 const router = require('express').Router();
 const { User } = require('../../models');
-const session = require('express-session');
-const bcrypt = require('bcrypt');
-const passport = require('passport');
-const localStrategy = require('passport-local').Strategy;
 
-
-
-passport.use(new localStrategy(function (email, password, done){
-    User.findOne({ username: email }, function(err, user){
-        if (err) return done(err);
-        if (!user) {
-            return done(null, false, { message: 'Incorrect Username' });
-        }
-        bcrypt.compare(password, user.password, function(err, res){
-            if (err) return done(err);
-            if (res === false) return done(null, false, { message: 'Incorrect password' });
-
-            return done(err, user);
-        })
-    })
-}))
-
-function success(req, res) {
-    const session = require('express-session')
-    req.session.save(() => {
-        console.log(dbUserData);
-        req.session.user_id = dbUserData.id;
-        req.session.username = dbUserData.username;
-        req.session.loggedIn = true;
-    
-        res.json(dbUserData);
-    });
-}
-
-
-passport.serializeUser(function (user_id, done){
-    done(null, user_id)
-});
-
-passport.deserializeUser(function (user_id, done){
-    User.findByPk(user_id, function(err, user_id){
-        done(err, user_id);
-    });
-});
 
 // GET /api/users
 router.get('/', (req, res) => {
@@ -78,40 +35,71 @@ router.get('/:id', (req, res) => {
 
 // POST /api/users
 router.post('/', (req, res) => {
-    bcrypt.genSalt(10, (err, salt) => {
-        if (err) return next(err);
-        bcrypt.hash(req.body.password, salt, function (err, hash){
-            if (err) return next(err);
-            User.create({
-                first_name: req.body.first_name,
-                last_name: req.body.last_name,
-                email: req.body.email,
-                address: req.body.address,
-                password: hash,
-                username: req.body.username
-            }) .then(dbUserData => {
-                    req.session.save(() => {
-                        console.log(dbUserData);
-                        req.session.user_id = dbUserData.id;
-                        req.session.username = dbUserData.username;
-                        req.session.loggedIn = true;
-                    
-                        res.json(dbUserData);
-                    });
-            }) .catch(err => {
-                console.log(err);
-                res.status(500).json(err);
-            });
-        })
-        
-    }); 
+
+    User.create({
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        email: req.body.email,
+        address: req.body.address,
+        password: req.body.password,
+        username: req.body.username
+    }).then(dbUserData => {
+        req.session.save(() => {
+            console.log(dbUserData);
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+
+            res.json(dbUserData);
+        });
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+
+})
+
+
+router.post('/login', (req, res) => {
+    // expects {email: 'lernantino@gmail.com', password: 'password1234'}
+    User.findOne({
+        where: {
+            email: req.body.email
+        }
+    }).then(dbUserData => {
+        if (!dbUserData) {
+            res.status(400).json({ message: 'No user with that email address!' });
+            return;
+        }
+
+        const validPassword = dbUserData.checkPassword(req.body.password);
+
+        if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect password!' });
+            return;
+        }
+
+        req.session.save(() => {
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+
+            res.json({ user: dbUserData, message: 'You are now logged in!' });
+        });
+    });
 });
 
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    }
+    else {
+        res.status(404).end();
+    }
+});
 
-router.post('/login', passport.authenticate('local', {
-    successRedirect: '/',
-    failureRedirect: '/login'
-}));
 
 router.post('/logout', function (req, res){
     if (req.session.loggedIn) {
--- server.js
+++ server.js
@@ -5,12 +5,12 @@ const exphbs        = require('express-handlebars');
 const fs            = require('fs');
 const flash         = require('express-flash');
 const passport      = require('passport');
-
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
 
 const sess = {
     secret: 'Super secret secret',
@@ -22,8 +22,11 @@ const sess = {
     })
 };
 
-app.use(passport.initialize());
 app.use(session(sess));
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(passport.authenticate("session"));
 
 const hbs = exphbs.create({});
 
@@ -33,12 +36,9 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(flash());
 
-
-const css = fs.readFileSync('./public/style.css', 'utf-8');
-// const loginJS = fs.readFileSync('./public/javascript/login.js', 'utf-8')
-// const logoutJS = fs.readFileSync('./public/javascript/logout.js', 'utf-8')
 
 app.use(require('./controllers/'));
 
